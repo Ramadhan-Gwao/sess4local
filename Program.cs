8ecfa9996e2162588035373a2da6a28e374766b1
Gwao <ramadhangwao@gmail.com>
2021-05-21 15:34:23 +0300

    sess4local update

--- Program.cs
+++ Program.cs
@@ -6,14 +6,16 @@ namespace project1
     {
         static void Main(string[] args)
         {
-            int currentYear = 2080;
             double earthAgeDays = 365.25;
             double earthAgeSec = 31557600;
             double newAgeSec = 631154000;
-            int time = 60*60*24;
+
+
             double result = (newAgeSec*earthAgeDays)/earthAgeSec;
             double result1 = result/earthAgeDays;
-            Console.WriteLine($"Earth Age: {result1} Earth-years old");
+            var result3 = Math.Floor(result1);
+            
+            Console.WriteLine($"Earth Age: {result3} Earth-years old");
         }
     }
 }
